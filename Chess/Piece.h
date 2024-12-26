c7114779caf86b6713d4bd02a4492e0e6a215d03
unknown <buzzi1234>
2024-12-26 18:09:44 +0200

    finished the piece class

--- Chess/Piece.h
+++ Chess/Piece.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+
+class Piece {
+private:
+	std::string _startIndex;
+	std::string _endIndex;
+	char _color;
+public:
+	virtual bool move() = 0;
+	virtual bool eat() = 0;
+	virtual bool inTheWay() = 0;
+	std::string getStartIndex();
+	std::string getEndIndex();
+	char getColor();
+	
+	void setStartIndex(std::string startIndex);
+	void setEndIndex(std::string endIndex);
+	void setColor(char color);
+
+};
